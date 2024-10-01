e4b11767393ade4af0ce38226a0b5dc213aeed0e
Leo <103794599@qq.com>
2024-10-01 11:29:14 +0800

    V0.1

--- apis.py
+++ apis.py
@@ -12,7 +12,7 @@ from datetime import datetime
 
 CUDA = None
 headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
     "Referer": "https://elife.fudan.edu.cn/app/",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "Accept-Encoding": "gzip, deflate, br",
