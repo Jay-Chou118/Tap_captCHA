946ae4a3ea87a562b29b4c9985807f79caa74854
Leo <103794599@qq.com>
2024-10-03 19:45:46 +0800

    V1.0.0.2 成功登录

--- Test_for_login/main.py
+++ Test_for_login/main.py
@@ -10,25 +10,30 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 
 url = 'https://www.bilibili.com'
 
-data = {'username': '',
-        'password': ''}
+data = {'username': '123',
+        'password': '123'}
 
-session = requests.session()
-cookie_jar = session.post(url=url, data=data, headers=headers).cookies
-cookie_t = requests.utils.dict_from_cookiejar(cookie_jar)
+# session = requests.session()
+# cookie_jar = session.post(url=url, data=data, headers=headers).cookies
+# cookie_t = requests.utils.dict_from_cookiejar(cookie_jar)
 
-print("cookies = " , cookie_t)
-# driver = webdriver.Chrome()
+# print("cookies = " , cookie_t)
+driver = webdriver.Chrome()
 # driver.get("https://www.bilibili.com")
-# driver.get('https://ehall.fudan.edu.cn/ywtb-portal/fudan/index.html#/hall')
+driver.get('https://ehall.fudan.edu.cn/ywtb-portal/fudan/index.html#/hall')
 
 # driver.find_element('.header-login-entry').clear()
-# driver.find_element(By.CLASS_NAME,'.header-login-entry').click()
+# driver.find_element(By.CLASS_NAME,'header-login-entry').click()\
+
+driver.find_element(By.CLASS_NAME,'btnloginbox').click()
 # cookies = driver.get_cookies()
 
-# time.sleep(2)
-# driver.find_element('')
+time.sleep(1)
+driver.find_element(By.ID,'username').send_keys(data['username'])
+driver.find_element(By.ID,'password').send_keys(data['password'])
 
+driver.find_element(By.ID,'idcheckloginbtn').click()
+time.sleep(1)
 # 添加延时或保持打开
 # time.sleep(10)  # 暂停10秒后关闭
-# input("Press Enter to close the browser·...")  # 按回车键后才关闭
+input("Press Enter to close the browser·...")  # 按回车键后才关闭
