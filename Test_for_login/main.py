9823e108930bb3f14a0c92c4f6971c4fda6cd1d3
guobinbin <email>
2024-10-09 21:39:32 +0800

    header修改

--- Test_for_login/main.py
+++ Test_for_login/main.py
@@ -11,8 +11,18 @@ from selenium.webdriver.common.by import By
 
 import requests
 
+
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'}
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
+    "Referer": "https://elife.fudan.edu.cn/app/",
+    # "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    # "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Language":"zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+    "Connection": "keep-alive",
+}
+
 
 url = 'https://ehall.fudan.edu.cn/ywtb-portal/fudan/index.html#/hall'
 
@@ -41,10 +51,10 @@ def login():
         # time.sleep(1)
         # 添加延时或保持打开
         # time.sleep(10)  # 暂停10秒后关闭
-        driver.find_element(By.CLASS_NAME, 'content').send_keys('体育')
-        time.sleep(1)
-        driver.find_element(By.CLASS_NAME, 'btn amp-theme').click()
-        time.sleep(1)
+        # driver.find_element(By.CLASS_NAME, 'content').send_keys('体育')
+        # time.sleep(1)
+        # driver.find_element(By.CLASS_NAME, 'btn amp-theme').click()
+        # time.sleep(1)
         input("Press Enter to close the browser·...")  # 按回车键后才关闭
 
 
