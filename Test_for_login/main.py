af1850bab121b1082d59a20b3250eac40711e4c6
郭彬彬guobinbin <guobinbin@xiaomi.com>
2024-10-16 16:03:44 +0800

    v1.0.5.6.4 [进行中]连续发送byte数组配置

--- Test_for_login/main.py
+++ Test_for_login/main.py
@@ -32,8 +32,8 @@ headers = {
 url = 'https://ehall.fudan.edu.cn/ywtb-portal/fudan/index.html#/hall'
 
 #输入自己的账号和密码
-data = {'username': '',
-        'password': ''}
+data = {'username': '23210720160',
+        'password': 'guoBB18876322223'}
 
 # 初始化找到的标志
 found = False
